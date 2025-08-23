3993bb479b5686d1ce5bfd9a1f0169c34372c988
yassine-bnj <yassine.benjeddou@etudiant-isi.utm.tn>
2025-08-24 01:43:46 +0200

    correct card error when deploy

--- app/components/homepage/projects/project-card.jsx
+++ app/components/homepage/projects/project-card.jsx
@@ -43,14 +43,14 @@ function ProjectCard({ project }) {
 
           <div className="ml-4 lg:ml-8 mr-2">
             <span className=" text-white">tools:</span>
-            <span className="text-gray-400">{` [`}&apos;</span>
+            <span className="text-gray-400">{" ["}&apos;</span>
             {
               project.tools.map((tag, i) => (
                 <React.Fragment key={i}>
                   <span className="text-amber-300">{tag}</span>
                   {
                     project.tools.length - 1 !== i &&
-                    <span className="text-gray-400">&apos; {`, `}&apos;</span>
+                    <span className="text-gray-400">&apos; , &apos;</span>
                   }
                 </React.Fragment>
               ))
