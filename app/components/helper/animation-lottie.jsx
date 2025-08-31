723758f870af1cac2cdd0bb3d73915743b0c023f
yassine-bnj <yassine.benjeddou@etudiant-isi.utm.tn>
2025-09-01 00:01:30 +0200

    correct document is not defined error

--- app/components/helper/animation-lottie.jsx
+++ app/components/helper/animation-lottie.jsx
@@ -1,19 +1,24 @@
 "use client";
 
+import { useEffect, useState } from "react";
 import Lottie from "lottie-react";
 
 const AnimationLottie = ({ animationPath, width }) => {
-  const defaultOptions = {
-    loop: true,
-    autoplay: true,
-    animationData: animationPath,
-    style: {
-      width: '95%',
-    }
-  };
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  if (!isClient) return null; // nothing on server
 
   return (
-    <Lottie {...defaultOptions} />
+    <Lottie
+      loop
+      autoplay
+      animationData={animationPath}
+      style={{ width: width || "95%" }}
+    />
   );
 };
 
