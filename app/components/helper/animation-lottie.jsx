c66c712d731cd34741db51c1c72fcd5d41e19acf
yassine-bnj <yassine.benjeddou@etudiant-isi.utm.tn>
2025-09-01 00:14:15 +0200

    correct document is not defined error

--- app/components/helper/animation-lottie.jsx
+++ app/components/helper/animation-lottie.jsx
@@ -1,25 +1,21 @@
 "use client";
 
-import { useEffect, useState } from "react";
-import Lottie from "lottie-react";
+import dynamic from "next/dynamic";
+
+// Dynamically import Lottie so it only runs on the client
+const Lottie = dynamic(() => import("lottie-react"), { ssr: false });
 
 const AnimationLottie = ({ animationPath, width }) => {
-  const [isClient, setIsClient] = useState(false);
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: animationPath,
+    style: {
+      width: width || "95%",
+    },
+  };
 
-  useEffect(() => {
-    setIsClient(true);
-  }, []);
-
-  if (!isClient) return null; // nothing on server
-
-  return (
-    <Lottie
-      loop
-      autoplay
-      animationData={animationPath}
-      style={{ width: width || "95%" }}
-    />
-  );
+  return <Lottie {...defaultOptions} />;
 };
 
 export default AnimationLottie;
